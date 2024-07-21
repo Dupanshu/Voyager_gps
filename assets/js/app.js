2e1ab4af3d508b6dd3538f47e73e68cfe2ea31ea
Dupanshu <dupanshurangray@gmail.com>
2024-07-21 15:34:46 -0500

    refreshing the css and js 21-7-24

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,7 +15,8 @@ const locate = new mapboxgl.Map({
     container: tracker,
     style: 'mapbox://styles/mapbox/streets-v11',
     zoom: 3,
-    center: [-90.96, 50.47]
+    center: [-90.96, 50.47],
+    attributionControl: false
 });
 
 locate.addControl(new mapboxgl.GeolocateControl({
@@ -27,7 +28,7 @@ locate.addControl(new mapboxgl.GeolocateControl({
 
 function putTracker(location) {
     const marker = utils.create('div');
-    marker.style.backgroundImage = 'url(./assets/media/tracker.png)';
+    marker.style.backgroundImage = 'url(./assets/media/car.gif)';
     marker.style.width = '50px';
     marker.style.height = '50px';
     marker.style.backgroundSize = 'cover';
